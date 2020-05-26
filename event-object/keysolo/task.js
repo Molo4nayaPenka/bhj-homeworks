36fa4517fd789b80fa4731c35f62d0986788125f
Olga Medvetskaya <olgamedvetskaya@yandex.ru>
2020-05-26 22:09:06 +0300

    keysolo commit 1

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -24,6 +24,14 @@ class Game {
       В случае правильного ввода слова вызываем this.success()
       При неправильном вводе символа - this.fail();
      */
+ 
+    document.addEventListener('keydown', (event) => {
+      if (event.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
