27362bbf0c177e953c651c371d6ce15248b691cf
Olga Medvetskaya <olgamedvetskaya@yandex.ru>
2020-05-30 20:37:53 +0300

    dropdown commit 2

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,22 +1,29 @@
 'use strict'
 
-const dropdownValue = document.querySelector('.dropdown__value');
-const dropdownList = document.querySelector('.dropdown__list');
+// собираем все выпадающие списки в массив
+const dropdowns = Array.from(document.querySelectorAll('.dropdown'));
 
-dropdownValue.onclick = () => {
-    if (dropdownList.classList.contains('dropdown__list_active')) {
-        dropdownList.classList.remove('dropdown__list_active');
-    } else {
-        dropdownList.classList.add('dropdown__list_active');
+// для каждого списка пишем обработчик 
+dropdowns.forEach(dropdown => {
+
+    const dropdownValue = dropdown.querySelector('.dropdown__value');
+    const dropdownList = dropdown.querySelector('.dropdown__list');
+    
+    dropdownValue.onclick = () => {
+        if (dropdownList.classList.contains('dropdown__list_active')) {
+            dropdownList.classList.remove('dropdown__list_active');
+        } else {
+            dropdownList.classList.add('dropdown__list_active');
+        }
     }
-}
-
-const dropdownItems = Array.from(dropdownList.querySelectorAll('.dropdown__item'));
-
-function changeDropdownValue(event) {
-    event.preventDefault();
-    dropdownValue.textContent = this.textContent;
-    dropdownList.classList.remove('dropdown__list_active');
-}
-
-dropdownItems.forEach((elem) => elem.addEventListener('click', changeDropdownValue));
+    
+    const dropdownItems = Array.from(dropdownList.querySelectorAll('.dropdown__item'));
+    
+    function changeDropdownValue(event) {
+        event.preventDefault();
+        dropdownValue.textContent = this.textContent;
+        dropdownList.classList.remove('dropdown__list_active');
+    }
+    
+    dropdownItems.forEach((elem) => elem.addEventListener('click', changeDropdownValue));
+});
