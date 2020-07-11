5dcab86b1b8ee6001a69bcad7f1462024c7dbcd5
Olga Medvetskaya <olgamedvetskaya@yandex.ru>
2020-07-11 14:29:16 +0300

    interests commit 2

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,20 +1,13 @@
 'use strict'
 
-const subZones = Array.from(document.querySelectorAll('.interests_active'));
+const interests = document.querySelectorAll('.interest__check');
+let parentElem;
+let childElem;
 
-subZones.forEach(subZone => {
-    const fullZone = subZone.closest('.interest');
-
-    fullZone.addEventListener('change', (e) => {
-        const subCheckboxes = Array.from(subZone.querySelectorAll('.interest__check'));
-        const mainCheckbox = fullZone.querySelector('.interest__check');
-
-        if (mainCheckbox.checked) {
-            subCheckboxes.forEach(subCheckbox => subCheckbox.checked = true);
-        } else if (!mainCheckbox.checked) {
-            subCheckboxes.forEach(subCheckbox => subCheckbox.checked = false);
-        } else if ((subCheckboxes.every(subCheckbox => subCheckbox.checked == true))) {   
-            mainCheckbox.checked = true;
-        }    
+interests.forEach(interest => {
+    interest.addEventListener('change', e => {
+        parentElem = e.target.closest('.interest'); // li class='interest'        
+        childElem = parentElem.querySelectorAll('.interest__check'); // input. Если главный interest - все чекбоксы с классом interest__check
+        childElem.forEach(elem => elem.checked = e.target.checked);
     })
 })
