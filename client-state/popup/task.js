d67f52b3e70c917ca3ff60d50ce9edd50e0a833a
Olga Medvetskaya <olgamedvetskaya@yandex.ru>
2020-07-31 23:14:54 +0300

    popup commit 2

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -6,17 +6,18 @@ function showModals() {
 
     modals.forEach(modal => {
         modal.classList.add('modal_active');
+
+        if (document.cookie.includes('isModalClosed=true')) {   
+            modal.classList.remove('modal_active');
+        }
     })
     
     closeBtns.forEach(closeBtn => {
         closeBtn.addEventListener('click', function (event) {
             const currentModal = event.target.closest('.modal_active');
             currentModal.classList.remove('modal_active');
-            // вообще не понимаю, что нужно писать в куки. что такое "информация о закрытии окна"?
-            document.cookie = 'key=' + encodeURIComponent(`${currentModal.classList}`);
-            console.log(document.cookie)
+            document.cookie = 'isModalClosed=true';
         })
-
     })
 }
 
